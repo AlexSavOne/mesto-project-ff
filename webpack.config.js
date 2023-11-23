50718e0e1856fd2580b78e11297f0e66f8d70f45
AlexS <OstinKarlail@yandex.ru>
2023-11-24 00:59:39 +0500

    Вроде закончил настройку вебпака.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: { main: './src/scripts/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -29,12 +30,30 @@ module.exports = {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
       },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        // Добавьте postcss-loader
+      'postcss-loader']
+      },
     ]
+  },
+  resolve: {
+    alias: {
+      images: path.resolve(__dirname, 'src/images'),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(), // подключение плагина для объединения файлов
   ]
 };
